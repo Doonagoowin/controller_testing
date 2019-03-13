80ed3ae7713e4c5861f110c53c2188d0a3b02ce6
Du Nguyen <nguyend33@gmail.com>
2019-03-12 19:55:24 -0600

    add controller test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,25 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'simplecov'
+SimpleCov.start
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+
+    with.test_framework :rspec
+
+    with.library :rails
+  end
+end
+
+RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+end
+
+require "devise"
+require "support/controller_macros"
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -25,7 +44,7 @@ require 'rspec/rails'
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
 begin
-  ActiveRecord::Migration.maintain_test_schema!
+  ActiveRecord::Migration.maintain_test_schema!                                 
 rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
