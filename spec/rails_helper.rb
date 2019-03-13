6d6563fc1583774d36aa125b9c9727a3bbb7f94d
Du Nguyen <nguyend33@gmail.com>
2019-03-12 21:02:08 -0600

    end of lecture

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -50,6 +50,10 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.extend ControllerMacros, type: :controller
+  
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
